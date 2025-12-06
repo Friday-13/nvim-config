df2bdf9031c0be897edb8cb9615bcf198e47e1f1
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-12-06 22:37:22 +0300

    refactor: migrate options

---
+++ lua/user/options.lua
@@ -0,0 +1,16 @@
+vim.opt.mouse = 'a' -- enable mouse
+vim.opt.number = true
+vim.opt.colorcolumn = '79'
+vim.opt.swapfile = false
+vim.opt.scrolloff = 7
+
+
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
+fileformat='unix'
+
+-- for tabulation
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth=2
