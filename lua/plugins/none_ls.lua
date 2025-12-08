b810ae41c1216bc12ff2b2c65e678b539f2662a9
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-12-08 03:01:34 +0300

    refactor: migrate none-ls plugin

---
+++ lua/plugins/none_ls.lua
@@ -0,0 +1,22 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+    debug = true,
+    sources = {
+        require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.code_actions.eslint_d"),
+        -- null_ls.builtins.diagnostics.eslint_d,
+        -- null_ls.builtins.code_actions.eslint_d,
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.prettier
+    },
+    on_attach = require("lsp.default_on_attach")
+    })
+  end
+}
