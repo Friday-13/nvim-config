8813b128b998dfc1cb416292bb971bd24218c8e1
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-12-08 03:12:11 +0300

    refactor: update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,17 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = "0.1.8",
     dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup{
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            "build",
+            "Drivers",
+            "Middlewares"
+          }
+        }
+      }
+    end
 }
