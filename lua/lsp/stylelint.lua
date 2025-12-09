3bd054b2403a466b5ab829a7beb5fe6532211c73
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-12-09 03:56:17 +0300

    refactor: migrate stylelint lsp

---
+++ lua/lsp/stylelint.lua
@@ -0,0 +1,14 @@
+vim.lsp.config("stylelint_lsp", {
+  on_attach = require("lsp.default_on_attach"),
+  flags = {
+    debounce_text_changes = 150,
+  },
+  settings = {
+    stylelintplus = {
+      -- autoFixOnSave = true,
+      -- autoFixOnFormat = true,
+    },
+  }
+})
+
+vim.lsp.enable("stylelint_lsp", true)
