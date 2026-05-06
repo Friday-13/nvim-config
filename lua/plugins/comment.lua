9fa252f0b38d38b509d46aecc112d04f065c7ac0
Dmitrii Samsonenko <samsondm97@gmail.com>
2026-05-06 17:53:50 +0300

    fix: add patch for comment plugin for nvim0.12

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,8 @@
 return {
     'numToStr/Comment.nvim',
+    -- patch for nvim 0.12
+    url = "https://github.com/faergeek/Comment.nvim",
+    branch = "nvim-0.12-compatibility",
     opts = {
     }
 }
