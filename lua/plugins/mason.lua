080e91c1df11d09e33a8e82ffdc2f52f37610c76
Dmitrii Samsonenko <samsondm97@gmail.com>
2026-07-19 23:06:29 +0300

    feat: add mason plugin

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    -- НЕ ленивый: mason должен прописать свой bin в PATH до того, как
+    -- стартуют LSP-серверы и none-ls полезет искать eslint_d/prettier/clang-format.
+    lazy = false,
+    priority = 100, -- раньше остальных eager-плагинов, но после colorscheme (1000)
+    build = ":MasonUpdate",
+    opts = {
+      -- PATH = "prepend" (дефолт): mason-версии бинарников имеют приоритет над системными
+      ui = {
+        border = "rounded",
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    lazy = false,
+    opts = {
+      -- имена — это mason-пакеты (не имена lspconfig-серверов)
+      ensure_installed = {
+        -- LSP
+        "pyright",
+        "typescript-language-server", -- ts_ls
+        "lua-language-server",        -- lua_ls
+        "html-lsp",                   -- html
+        "css-lsp",                    -- cssls
+        "stylelint-lsp",              -- stylelint_lsp
+        "clangd",
+        -- инструменты none-ls
+        "eslint_d",
+        "prettier",
+        "black",
+        "isort",
+        "clang-format",
+      },
+      -- ставить недостающее при старте, но не запускать блокирующе
+      run_on_start = true,
+      -- небольшая задержка, чтобы не мешать первичной отрисовке
+      start_delay = 3000,
+    },
+  },
+}
